19c460b66b41d5264cdd36766f92ebd7fedaa4f7
tunga3109 <tunga3109@gmail.com>
2023-04-10 01:24:27 +0300

    added changes

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
+from typing import List
 from fastapi import FastAPI, Response, Path
 from fastapi.responses import FileResponse
+from pydantic.fields import Field
+from pydantic.main import BaseModel
 
 app = FastAPI()
 
-@app.get('/file', response_class=FileResponse)
-def read_root():
-    return 'index.html'
+
+class Trade(BaseModel):
+    name: str 
+    surname: str
+    age: int = Field(gt=18, lt=100, title='Age')
+    
+users = [
+    {'name': 'Anton', 'Surname': 'Zyanouchik', 'age': 25},
+    ]
+
+@app.post('/users')
+def add_user(user: List[Trade]):
+    users.extend(user)
+    return {'data': users}
+
