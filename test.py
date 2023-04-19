b5cf91e5fe3d0fb7394182b45f4a0363a76928de
tunga3109 <tunga3109@gmail.com>
2023-04-20 00:39:07 +0300

    test

--- test.py
+++ test.py
@@ -2,15 +2,14 @@ from fastapi import FastAPI
 from typing import List, Optional
 from pydantic import BaseModel
 
+app = FastAPI()
+
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
     tags: List[str] = []
-
-
-app = FastAPI()
 
 items = []
 
